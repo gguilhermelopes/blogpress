de1dcd3a4d742081949be3ae93f5a03462f3deac
gguilhermelopes <guilhermelopes275@gmail.com>
2023-02-21 23:33:25 -0300

    build: addition, deletion and edit of categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,11 +15,48 @@ categoriesController.post("/categories/save", (req, res) => {
       title,
       slug: slugify(title).toLowerCase(),
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     });
   } else {
     res.redirect("/admin/categories/new");
   }
+});
+
+categoriesController.get("/admin/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories });
+  });
+});
+
+categoriesController.post("/categories/delete", (req, res) => {
+  const { id } = req.body;
+  if (id && !isNaN(id)) {
+    Category.destroy({
+      where: {
+        id: id,
+      },
+    }).then(() => {
+      res.redirect("/admin/categories");
+    });
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+categoriesController.get("/admin/categories/edit/:id", (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) res.redirect("/admin/categories");
+  Category.findByPk(id)
+    .then((category) => {
+      if (category) {
+        res.render("admin/categories/edit", { category });
+      } else {
+        res.redirect("/admin/categories");
+      }
+    })
+    .catch((error) => {
+      res.redirect("/admin/categories");
+    });
 });
 
 export default categoriesController;
