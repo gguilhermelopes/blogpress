a30e38c19e5f39daf98b65de6363812f66491d2c
gguilhermelopes <guilhermelopes275@gmail.com>
2023-02-23 14:51:41 -0300

    feat: articles delete

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,54 @@
 import express from "express";
+import Category from "../categories/Category.js";
+import Article from "../articles/Article.js";
+import slugify from "slugify";
+
 const articlesController = express.Router();
 
-articlesController.get("/articles", (req, res) => {
-  res.send("Rota de Artigos");
+articlesController.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [
+      {
+        model: Category,
+      },
+    ],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles });
+  });
+});
+
+articlesController.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories });
+  });
+});
+
+articlesController.post("/articles/save", (req, res) => {
+  const { title, body, category } = req.body;
+
+  Article.create({
+    title,
+    slug: slugify(title).toLowerCase(),
+    body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect("/admin/articles");
+  });
+});
+
+articlesController.post("/articles/delete", (req, res) => {
+  const { id } = req.body;
+  if (id && !isNaN(id)) {
+    Article.destroy({
+      where: {
+        id: id,
+      },
+    }).then(() => {
+      res.redirect("/admin/articles");
+    });
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 export default articlesController;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ import Sequelize from "sequelize";
 const connection = new Sequelize("blogpress", "root", "admin", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 export default connection;
